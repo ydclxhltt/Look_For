999a8eb449f571b4dab44f53fa78e05095438c43
ydclxhltt <ydclxhltt@users.noreply.github.com>
2015-01-25 12:34:09 +0800

    文件移动

---
+++ LookFor/LookForViewControllers/LBSViewController/LookForSafeTravelViewController.h
@@ -0,0 +1,13 @@
+//
+//  LookForSafeTravelViewController.h
+//  LookFor
+//
+//  Created by chenmingguo on 15-1-8.
+//  Copyright (c) 2015年 chenmingguo. All rights reserved.
+//
+
+#import "BasicViewController.h"
+
+@interface LookForSafeTravelViewController : BasicViewController
+
+@end
