0c19cae14e6e55f6cad82ca8f7d0207c53562d91
ydclxhltt <ydclxhltt@users.noreply.github.com>
2015-01-26 20:56:35 +0800

    修改设置

--- LookFor/LookForViewControllers/UserViewController/LookForSettingViewController.m
+++ LookFor/LookForViewControllers/UserViewController/LookForSettingViewController.m
@@ -7,19 +7,153 @@
 //
 
 #import "LookForSettingViewController.h"
+#import "LookForSettingTableViewCell.h"
 
-@interface LookForSettingViewController ()
+@interface LookForSettingViewController ()<UITableViewDataSource, UITableViewDelegate>
 
 @end
 
 @implementation LookForSettingViewController
 
+- (void)loadView {
+    [super loadView];
+    [self initView];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self setNavBarItemWithTitle:@"取消"
+                     navItemType:LeftItem
+                    selectorName:@"handleCancel"];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark -custom
+- (void)initView {
+  
+    [self addTableViewWithFrame:CGRectMake(0, 0, MAIN_SCREEN_SIZE.width, self.view.frame.size.height)
+                      tableType:UITableViewStylePlain
+                  tableDelegate:self];
+    self.table.separatorColor = [UIColor clearColor];
+}
+
+#pragma mark -handle
+- (void)handleCancel {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)handleLogOut {
+
+}
+#pragma -message
+#pragma mark - TableView Datasource And Delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return 4;
+    } else {
+        return 2;
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *displayCellIdentifier = @"LookForSelectFriendTableViewCell";
+    
+    NSUInteger row = [indexPath row];
+    NSUInteger section = [indexPath section];
+    
+    LookForSettingTableViewCell *cell = (LookForSettingTableViewCell *) [tableView dequeueReusableCellWithIdentifier:displayCellIdentifier];
+    if (cell == nil) {
+        cell = (LookForSettingTableViewCell *)[[LookForSettingTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier: displayCellIdentifier];
+        cell.selectionStyle = UITableViewCellSelectionStyleGray;
+    }
+    
+    if (section == 0) {
+        if (row == 0) {
+            cell.titleText = @"地图展示方式";
+        } else if (row == 1) {
+            cell.titleText = @"闭眼权限设置";
+
+        } else if (row == 2) {
+            cell.titleText = @"常用地址";
+
+        } else {
+            cell.titleText = @"添加默认联系人";
+
+        }
+    } else {
+        if (row == 0) {
+            cell.titleText = @"版本更新";
+
+        } else {
+            cell.titleText = @"关于我们";
+        
+        }
+    }
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 15.0f;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    
+    UIView *head = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 15)];
+    head.backgroundColor = kTableViewGrayColor;
+    return head;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    
+    if (section == 1) {
+        return 60;
+    }else {
+        return  0;
+    }
+    
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    if (section == 1) {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, MAIN_SCREEN_SIZE.width, 60)];
+        view.backgroundColor = kTableViewGrayColor;
+        
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.frame = CGRectMake(0, 20, MAIN_SCREEN_SIZE.width, 40);
+        button.backgroundColor = [UIColor redColor];
+        [button addTarget:self
+                   action:@selector(handleLogOut)
+         forControlEvents:UIControlEventTouchUpInside];
+        
+        [view addSubview:button];
+        return view;
+    } else {
+        return nil;
+    }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSUInteger row = [indexPath row];
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+    
 }
 
 @end
