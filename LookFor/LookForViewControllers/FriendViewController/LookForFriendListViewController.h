06f328fa03a7d591ac776540af3472880604838c
ydclxhltt <ydclxhltt@users.noreply.github.com>
2015-02-11 15:48:41 +0800

    增加还有列表界面

---
+++ LookFor/LookForViewControllers/FriendViewController/LookForFriendListViewController.h
@@ -0,0 +1,13 @@
+//
+//  LookForFriendListViewController.h
+//  LookFor
+//
+//  Created by clei on 15/2/11.
+//  Copyright (c) 2015年 LookFor. All rights reserved.
+//
+
+#import "BasicViewController.h"
+
+@interface LookForFriendListViewController : BasicViewController
+
+@end
---
+++ LookFor/LookForViewControllers/FriendViewController/LookForFriendListViewController.m
@@ -0,0 +1,37 @@
+//
+//  LookForFriendListViewController.m
+//  LookFor
+//
+//  Created by clei on 15/2/11.
+//  Copyright (c) 2015年 LookFor. All rights reserved.
+//
+
+#import "LookForFriendListViewController.h"
+
+@interface LookForFriendListViewController ()
+
+@end
+
+@implementation LookForFriendListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
